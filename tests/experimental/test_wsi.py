8be60e4875b00debc0969039c9a7e4d278538c9b
Lucas Diedrich <diedrich@biochem.mpg.de>
2024-11-25 08:24:31 +0100

    Updated test files

--- tests/experimental/test_wsi.py
+++ tests/experimental/test_wsi.py
@@ -1,7 +1,8 @@
-from spatialdata_io.experimental.wsi import read_wsi, _create_tiles
-import openslide
 import numpy as np
+import openslide
 import pytest
+
+from spatialdata_io.experimental.wsi import _create_tiles, read_wsi
 
 
 @pytest.mark.parametrize(
@@ -17,8 +18,8 @@ def test__create_tiles(
     dimensions: tuple[int, int],
     tile_size: tuple[int, int],
     output_shape: tuple[int, int],
-    output_max,
-):
+    output_max: tuple[int, int],
+) -> None:
     array, xmax, ymax = _create_tiles(dimensions, tile_size)
     assert array.shape == output_shape
     assert (xmax == output_max[0]) & (ymax == output_max[1])
@@ -28,13 +29,10 @@ def test__create_tiles(
     ("dataset", "xmin", "xmax", "ymin", "ymax"),
     [
         # Use weird coords as most of the WSI is empty
-        # ("DigitalSlide_A6M_7S_1.mrxs", 75000, 76000, 118000, 119000),
         ("Mirax2.2-4-PNG.mrxs", 0, 0, 1000, 1000),
     ],
 )
-def test_read_wsi_pyramid(
-    dataset: str, xmin: int, xmax: int, ymin: int, ymax: int
-) -> None:
+def test_read_wsi_pyramid(dataset: str, xmin: int, xmax: int, ymin: int, ymax: int) -> None:
     """Test whether image can be loaded"""
     path = f"./data/wsi/mirax/{dataset}"
     image_model = read_wsi(path, pyramidal=True)
@@ -44,9 +42,7 @@ def test_read_wsi_pyramid(
 
     # Read image directly with openslide
     slide = openslide.OpenSlide(path)
-    ref_image = np.array(
-        slide.read_region((xmin, ymin), level=0, size=(xmax - xmin, ymax - ymin))
-    )
+    ref_image = np.array(slide.read_region((xmin, ymin), level=0, size=(xmax - xmin, ymax - ymin)))
 
     assert (test_image == ref_image.T).all()
 
@@ -55,14 +51,14 @@ def test_read_wsi_pyramid(
     "chunk_size",
     [(1000, 1000), (10000, 10000), (10000, 5000)],
 )
-def test_read_wsi_chunksize(chunk_size: bool) -> None:
+def test_read_wsi_chunksize(chunk_size: tuple[int, int]) -> None:
     """Test whether chunking works"""
     path = "./data/wsi/mirax/Mirax2.2-4-PNG.mrxs"
     image_model = read_wsi(path, chunk_size=chunk_size, pyramidal=False)
 
     assert all(
-        [
+        (
             ((x == chunk_size[0]) & (y == chunk_size[1]))
             for x, y in zip(image_model.chunksizes["x"], image_model.chunksizes["y"])
-        ]
+        )
     )
