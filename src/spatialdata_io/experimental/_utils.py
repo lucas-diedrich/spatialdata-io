860777544591c8b553dbd1100b85efbe5428f639
Lucas Diedrich <diedrich@biochem.mpg.de>
2024-11-26 08:54:47 +0100

    Debugged, corrected row/col definitions and order in which the nested list is created

--- src/spatialdata_io/experimental/_utils.py
+++ src/spatialdata_io/experimental/_utils.py
@@ -7,36 +7,48 @@ from dask import delayed
 from numpy.typing import NDArray
 
 
-def _create_tiles(dimensions: tuple[int, int], tile_size: tuple[int, int]) -> tuple[NDArray, int, int]:
-    """Tiling of WSI in digestible, rectangular chunks
+def _create_tiles(
+    dimensions: tuple[int, int],
+    tile_size: tuple[int, int],
+    min_coordinates: tuple[int, int] = (0, 0),
+) -> NDArray[np.int_]:
+    """Create rectangular tiles for a given image size.
 
     Parameters
     ----------
-    dimensions
-        Size of WSI in (x, y)
-    tile_size
-        Size of individual tiles in (x, y)
+    dimensions : tuple[int, int]
+        Size of the image in (width, height).
+    tile_size : tuple[int, int]
+        Size of individual tiles in (width, height).
+    min_coordinates : tuple[int, int], optional
+        Minimum coordinates in the image, defaults to (0, 0).
 
     Returns
     -------
-    tuple[NDArray, int, int]
-        - Array in the shape (n_tile_x, n_tile_y, 2) where the last dimension indicates the upper left corner of each tile (x, y).
-        - maximum value in x coordinates
-        - maximum value in y coordinates
-
+    np.ndarray
+        Array of shape (n_tiles_x, n_tiles_y, 4), where each entry defines a tile
+        as (x, y, width, height).
     """
-    n_tile_row = int(np.ceil(dimensions[0] / tile_size[0]))
-    n_tile_col = int(np.ceil(dimensions[1] / tile_size[1]))
+    # Calculate tile sizes and positions
+    widths = np.full(dimensions[0] // tile_size[0], tile_size[0])
+    remainder_x = dimensions[0] % tile_size[0]
+    if remainder_x > 0:
+        widths = np.append(widths, remainder_x)
 
-    row_max = int(n_tile_row * tile_size[0])
-    col_max = int(n_tile_col * tile_size[1])
+    heights = np.full(dimensions[1] // tile_size[1], tile_size[1])
+    remainder_y = dimensions[1] % tile_size[1]
+    if remainder_y > 0:
+        heights = np.append(heights, remainder_y)
 
-    # Get all grid points
-    cols, rows = np.meshgrid(np.arange(0, row_max, tile_size[0]), np.arange(0, col_max, tile_size[1]))
+    x_positions = min_coordinates[0] + np.cumsum(np.r_[0, widths[:-1]])
+    y_positions = min_coordinates[1] + np.cumsum(np.r_[0, heights[:-1]])
 
-    tile_coords = np.stack([rows.T, cols.T], axis=-1)
-
-    return tile_coords, row_max, col_max
+    # Generate the tiles
+    tiles = np.array(
+        [[[x, y, w, h] for y, h in zip(y_positions, heights)] for x, w in zip(x_positions, widths)],
+        dtype=int,
+    )
+    return tiles
 
 
 @delayed
@@ -44,7 +56,6 @@ def _chunk_factory(
     func: Callable[..., NDArray],
     slide: Any,
     coords: NDArray,
-    size: tuple[int, int],
     **func_kwargs: Mapping[str, Any],
 ) -> list[list[NDArray]]:
     """Abstract factory method to tile a large microscopy image.
@@ -56,8 +67,8 @@ def _chunk_factory(
     slide
         Slide image in format compatible with func
     coords
-        Coordinates of the upper left corner of the image in formt (n_row_x, n_row_y, 2)
-        where the last dimension indicates the position in format (x, y)
+        Coordinates of the upper left corner of the image in formt (n_row_x, n_row_y, 4)
+        where the last dimension defines the rectangular tile in format (x, y, width, height)
     func_kwargs
         Additional keyword arguments passed to func
     """
@@ -68,8 +79,16 @@ def _chunk_factory(
     # Outer list becomes dim=-2 (cols)
     # see dask.array.block
     chunks = [
-        [func(slide, coords=coords[x, y], size=size, **func_kwargs) for x in range(coords.shape[0])]
-        for y in range(coords.shape[1])
+        [
+            func(
+                slide,
+                coords=coords[x, y, [0, 1]],
+                size=coords[x, y, [2, 3]],
+                **func_kwargs,
+            )
+            for y in range(coords.shape[1])
+        ]
+        for x in range(coords.shape[0])
     ]
 
     return chunks
@@ -78,4 +97,4 @@ def _chunk_factory(
 @delayed
 def _assemble_delayed(chunks: list[list[NDArray]]) -> NDArray:
     """Assemble chunks (delayed)"""
-    return da.block(chunks)
+    return da.block(chunks, allow_unknown_chunksizes=True)
--- src/spatialdata_io/experimental/czi.py
+++ src/spatialdata_io/experimental/czi.py
@@ -1,9 +1,9 @@
 from collections.abc import Mapping
-from typing import Any
+from enum import Enum
+from typing import Any, Optional
 
 import dask.array as da
 import numpy as np
-from dask import delayed
 from numpy.typing import NDArray
 from pylibCZIrw import czi as pyczi
 from spatialdata.models import Image2DModel
@@ -11,12 +11,33 @@ from spatialdata.models import Image2DModel
 from ._utils import _assemble_delayed, _chunk_factory, _create_tiles
 
 
-@delayed
+class CZIPixelType(Enum):
+    """Features of CZI pixel types
+
+    Stores dimensionality, data type, and channel names of CZI pixel types
+    as class for simplified access.
+    Documented pixel types https://zeiss.github.io/libczi/accessors.html
+    """
+
+    Gray8 = (1, np.uint8, None)
+    Gray16 = (1, np.uint16, None)
+    Gray32Float = (1, np.float32, None)
+    Bgr24 = (3, np.uint8, "rgb")
+    Bgr48 = (3, np.uint16, "rgb")
+    Bgr96Float = (3, np.float32, "rgb")
+    Invalid = (np.nan, np.nan, np.nan)
+
+    def __init__(self, dimensionality: int, dtype: type, c_coords: Optional[str]) -> None:
+        self.dimensionality = dimensionality
+        self.dtype = dtype
+        self.c_coords = c_coords
+
+
 def _get_img(
     slide: pyczi.CziReader,
     coords: tuple[int, int],
     size: tuple[int, int],
-    channel: int,
+    channel: int = 0,
 ) -> NDArray:
     """Return numpy array of slide region
 
@@ -59,7 +80,14 @@ def _get_img(
     return np.array(img).T
 
 
-def read_czi(path: str, chunk_size: tuple[int, int] = (10000, 10000), **kwargs: Mapping[str, Any]) -> Image2DModel:
+def read_czi(
+    path: str,
+    chunk_size: tuple[int, int] = (10000, 10000),
+    channel: int = 0,
+    timepoint: int = 0,
+    z_stack: int = 0,
+    **kwargs: Mapping[str, Any],
+) -> Image2DModel:
     """Read .czi to Image2DModel
 
     Uses the CZI API to read .czi Carl Zeiss image format to spatialdata Image format
@@ -72,34 +100,43 @@ def read_czi(path: str, chunk_size: tuple[int, int] = (10000, 10000), **kwargs: 
         Size of the individual regions that are read into memory during the process
     kwargs
         Keyword arguments passed to Image2DModel.parse
+    channel
+        If multiple channels are available, select these channels
+    timepoint
+        If timeseries, select the given index (defaults to 0 [first])
+    z_stack
+        If z_stack, defaults to the given stack/index (defaults to 0 [first])
 
     Returns
     -------
     Image2DModel
     """
-    with pyczi.open_czi(path) as czidoc_r:
-        # Read dimensions
-        dimensions = czidoc_r.total_bounding_rectangle
-        try:
-            channels = dimensions["C"]
-        except KeyError:
-            raise KeyError("No channel information in .czi image")
+    czidoc_r = pyczi.CziReader(path)
 
-        # Define coordinates for chunkwise loading of the slide
-        chunk_coords, xmax, ymax = _create_tiles(dimensions=dimensions, tile_size=chunk_size)
+    # Read dimensions
+    xmin, ymin, width, height = czidoc_r.total_bounding_rectangle
 
-        chunks = [
-            _chunk_factory(
-                _get_img,
-                slide=czidoc_r,
-                coords=chunk_coords,
-                size=chunk_size,
-                channel=c,
-            )
-            for c in range(*channels)
-        ]
+    # We need to know the pixel type to infer the dimensionality of the image
+    pixel_type = czidoc_r.get_channel_pixel_type(channel)
+
+    # Define coordinates for chunkwise loading of the slide
+    chunk_coords = _create_tiles(dimensions=(width, height), tile_size=chunk_size, min_coordinates=(xmin, ymin))
+
+    # TODO Currently, only 1 channel [RGB/grayscale] is read - it might be complicated to read multiple channels
+    # if there are both brightfield (RGB) + fluorescence/grayscale images in the same file
+    chunks = _chunk_factory(_get_img, slide=czidoc_r, coords=chunk_coords, channel=channel)
 
     array_ = _assemble_delayed(chunks)
-    array = da.from_delayed(array_, shape=(4, ymax, xmax), dtype=np.uint8).rechunk(chunks=(4, *chunk_size[::-1]))
 
-    return Image2DModel.parse(array, dims="cyx", c_coords="rgba", chunks=chunk_size, **kwargs)
+    array = da.from_delayed(
+        array_,
+        shape=(CZIPixelType[pixel_type].dimensionality, width, height),
+        dtype=CZIPixelType[pixel_type].dtype,
+    )
+
+    return Image2DModel.parse(
+        array,
+        dims="cyx",
+        c_coords=CZIPixelType[pixel_type].c_coords,
+        **kwargs,
+    )
