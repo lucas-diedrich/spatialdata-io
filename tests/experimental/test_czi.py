94371d3c962f0397ac71f0bfb752dcac1573522d
Lucas Diedrich <diedrich@biochem.mpg.de>
2024-11-26 11:16:23 +0100

    Added tests for multichannel images

--- tests/experimental/test_czi.py
+++ tests/experimental/test_czi.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 import pytest
 from pylibCZIrw import czi as pyczi
 
@@ -31,3 +33,34 @@ def test_read_czi(dataset: str, xmin: int, ymin: int, width: int, height: int) -
 
     assert array.shape[1:] == (total_width, total_height)
     assert (img_test == img_ref).all()
+
+
+@pytest.mark.parametrize(
+    ("dataset", "xmin", "ymin", "width", "height", "channels", "result_dim"),
+    [
+        ("zeiss_multi-channel.czi", 0, 0, 1000, 1000, 0, 1),
+        ("zeiss_multi-channel.czi", 0, 0, 1000, 1000, [0], 1),
+        ("zeiss_multi-channel.czi", 0, 0, 1000, 1000, [0, 1], 2),
+    ],
+)
+def test_read_czi_multichannel(
+    dataset: str,
+    xmin: int,
+    ymin: int,
+    width: int,
+    height: int,
+    channels: Union[int, list[int]],
+    result_dim: int,
+) -> None:
+    path = f"./data/zeiss/{dataset}"
+
+    # Get reference with CZI reader
+    czidoc_r = pyczi.CziReader(path)
+
+    # Returns numpy array with shape (y, x, c)
+    xmin_czi, ymin_czi, total_width, total_height = czidoc_r.total_bounding_rectangle
+
+    # Test function
+    array = read_czi(path, channels=channels)
+
+    assert array.shape == (result_dim, total_width, total_height)
