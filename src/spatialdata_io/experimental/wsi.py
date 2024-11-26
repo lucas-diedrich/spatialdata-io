4896e5dc304c7140fea37c8491629bc63c3b5be3
Lucas Diedrich <diedrich@biochem.mpg.de>
2024-11-26 11:45:03 +0100

    Refactored read_wsi to leverage shared properties with read_czi

--- src/spatialdata_io/experimental/wsi.py
+++ src/spatialdata_io/experimental/wsi.py
@@ -7,37 +7,7 @@ from dask import delayed
 from numpy.typing import NDArray
 from spatialdata.models import Image2DModel
 
-
-def _create_tiles(dimensions: tuple[int, int], tile_size: tuple[int, int]) -> tuple[NDArray, int, int]:
-    """Tiling of WSI in digestible, rectangular chunks
-
-    Parameters
-    ----------
-    dimensions
-        Size of WSI in (x, y)
-    tile_size
-        Size of individual tiles in (x, y)
-
-    Returns
-    -------
-    tuple[np.NDArray, int, int]
-        - Array in the shape (n_tile_x, n_tile_y, 2) where the last dimension indicates the upper left corner of each tile (x, y).
-        - maximum value in x coordinates
-        - maximum value in y coordinates
-
-    """
-    n_tile_row = int(np.ceil(dimensions[0] / tile_size[0]))
-    n_tile_col = int(np.ceil(dimensions[1] / tile_size[1]))
-
-    row_max = int(n_tile_row * tile_size[0])
-    col_max = int(n_tile_col * tile_size[1])
-
-    # Get all grid points
-    cols, rows = np.meshgrid(np.arange(0, row_max, tile_size[0]), np.arange(0, col_max, tile_size[1]))
-
-    tile_coords = np.stack([rows.T, cols.T], axis=-1)
-
-    return tile_coords, row_max, col_max
+from ._utils import _assemble_delayed, _chunk_factory, _create_tiles
 
 
 @delayed
@@ -71,12 +41,6 @@ def _get_img(
 
     # Return image in (c, y, x) format
     return np.array(img).T
-
-
-@delayed
-def _assemble_delayed(chunks: list[list[NDArray]]) -> NDArray:
-    """Assemble chunks (delayed)"""
-    return da.block(chunks)
 
 
 def read_wsi(path: str, chunk_size: tuple[int, int] = (10000, 10000), pyramidal: bool = True) -> Image2DModel:
@@ -130,23 +94,13 @@ def read_wsi(path: str, chunk_size: tuple[int, int] = (10000, 10000), pyramidal:
         ]
 
     # Define coordinates for chunkwise loading of the slide
-    chunk_coords, xmax, ymax = _create_tiles(dimensions=dimensions, tile_size=chunk_size)
+    chunk_coords = _create_tiles(dimensions=dimensions, tile_size=chunk_size, min_coordinates=(0, 0))
 
-    # Collect each delayed chunk as item in list of list
-    # Inner list becomes dim=-1 (rows)
-    # Outer list becomes dim=-2 (cols)
-    # see dask.array.block
-    chunks = [
-        [
-            _get_img(slide=slide, coords=chunk_coords[x, y], size=chunk_size, level=0)
-            for x in range(chunk_coords.shape[0])
-        ]
-        for y in range(chunk_coords.shape[1])
-    ]
+    chunks = _chunk_factory(_get_img, slide=slide, coords=chunk_coords, level=0)
 
-    # Delayed
+    # Assemble into a single dask array
     array_ = _assemble_delayed(chunks)
-    array = da.from_delayed(array_, shape=(4, ymax, xmax), dtype=np.uint8).rechunk(chunks=(4, *chunk_size[::-1]))
+    array = da.from_delayed(array_, shape=(4, *dimensions[::-1]), dtype=np.uint8).rechunk()
 
     return Image2DModel.parse(
         array,
