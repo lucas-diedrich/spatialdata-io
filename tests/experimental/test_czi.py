11dbddccf4eb6b7d082918ad866e948d7737bc36
Lucas Diedrich <diedrich@biochem.mpg.de>
2024-11-26 09:58:55 +0100

    Updated and verified data-specific tests

---
+++ tests/experimental/test_czi.py
@@ -0,0 +1,33 @@
+import pytest
+from pylibCZIrw import czi as pyczi
+
+from spatialdata_io.experimental.czi import read_czi
+
+
+@pytest.mark.parametrize(
+    ("dataset", "xmin", "ymin", "width", "height"),
+    [
+        ("kabatnik2023_20211129_C1.czi", -170000, 40000, 10000, 10000),
+        # Asymmetric
+        ("zeiss_multi-channel.czi", 0, 0, 1000, 1000),
+    ],
+)
+def test_read_czi(dataset: str, xmin: int, ymin: int, width: int, height: int) -> None:
+    path = f"./data/zeiss/{dataset}"
+
+    # Get reference with CZI reader
+    czidoc_r = pyczi.CziReader(path)
+
+    # Returns numpy array with shape (y, x, c)
+    xmin_czi, ymin_czi, total_width, total_height = czidoc_r.total_bounding_rectangle
+    img_ref = czidoc_r.read(plane={"C": 0, "T": 0, "Z": 0}, roi=(xmin, ymin, width, height))
+
+    # Test function
+    array = read_czi(path)
+
+    # Coordinate systems are not aligned, modify roi
+    x, y = xmin - xmin_czi, ymin - ymin_czi
+    img_test = array[:, x : x + width, y : y + height].compute().transpose("x", "y", "c")
+
+    assert array.shape[1:] == (total_width, total_height)
+    assert (img_test == img_ref).all()
